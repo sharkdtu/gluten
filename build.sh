76f68fbd56c8133510c85b00226cf3cbf797de96
sharkdtu <sharkdtu@tencent.com>
2024-08-12 21:28:32 +0800

    [FEATURE-#1] Add script to build on TencentOS 3

---
+++ build.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+set -exu
+
+PROJECT_DIR="$(cd "$(dirname $0)"; pwd)"
+VELOX_REPO="https://private:5e2YGwdoruE9j30GRyRh@git.woa.com/wxg-bigdata/spark/velox.git"
+
+CPP_BUILD_CMD="./dev/builddeps-veloxbe.sh --velox_repo=${VELOX_REPO} --build_jemalloc=ON --enable_vcpkg=ON --enable_hdfs=ON $@"
+JAVA_BUILD_CMD="mvn clean package -Pbackends-velox -Pspark-3.3 -Piceberg -DskipTests"
+
+DOCKER_IMG="gluten-velox-dev"
+
+docker build \
+  --network=host \
+  -t ${DOCKER_IMG} \
+  -f ${PROJECT_DIR}/dev/docker/Dockerfile .
+
+docker run -it --rm \
+  --network=host \
+  -v ${PROJECT_DIR}:${PROJECT_DIR} \
+  ${DOCKER_IMG} \
+  sh -c \
+  "cd ${PROJECT_DIR} && \
+  export MAVEN_OPTS=-Dmaven.repo.local=${PROJECT_DIR}/.mvn-build && \
+  source /opt/rh/gcc-toolset-9/enable && \
+  ${CPP_BUILD_CMD} && \
+  ${JAVA_BUILD_CMD}"
