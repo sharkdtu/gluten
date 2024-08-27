48ddcae9eeaf1bf4a674a14daa996fa3fb753bc4
Yang Zhang <yangchuan.zy@alibaba-inc.com>
2024-08-27 15:46:07 +0800

    [VL] Add write IO metrics for WriteFiles (#7011)

--- cpp/core/jni/JniWrapper.cc
+++ cpp/core/jni/JniWrapper.cc
@@ -178,7 +178,7 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   metricsBuilderClass = createGlobalClassReferenceOrError(env, "Lorg/apache/gluten/metrics/Metrics;");
 
   metricsBuilderConstructor = getMethodIdOrError(
-      env, metricsBuilderClass, "<init>", "([J[J[J[J[J[J[J[J[J[JJ[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J)V");
+      env, metricsBuilderClass, "<init>", "([J[J[J[J[J[J[J[J[J[JJ[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J[J)V");
 
   nativeColumnarToRowInfoClass =
       createGlobalClassReferenceOrError(env, "Lorg/apache/gluten/vectorized/NativeColumnarToRowInfo;");
@@ -504,6 +504,7 @@ JNIEXPORT jobject JNICALL Java_org_apache_gluten_vectorized_ColumnarBatchOutIter
       longArray[Metrics::kIoWaitTime],
       longArray[Metrics::kPreloadSplits],
       longArray[Metrics::kPhysicalWrittenBytes],
+      longArray[Metrics::kWriteIOTime],
       longArray[Metrics::kNumWrittenFiles]);
 
   JNI_METHOD_END(nullptr)
