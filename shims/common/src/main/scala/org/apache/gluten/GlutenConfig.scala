5cf4ea69f925b3b5d1d327cf2ed120e82ea08d4c
surnaik <srnk@google.com>
2024-09-26 06:40:27 +0530

    [VL] Minor: Fix documentation of flushable partial aggregate (#7353)

--- shims/common/src/main/scala/org/apache/gluten/GlutenConfig.scala
+++ shims/common/src/main/scala/org/apache/gluten/GlutenConfig.scala
@@ -1827,8 +1827,8 @@ object GlutenConfig {
     buildConf("spark.gluten.sql.columnar.backend.velox.abandonPartialAggregationMinPct")
       .internal()
       .doc(
-        "If partial aggregation input rows number greater than this value, "
-          + " partial aggregation may be early abandoned. Note: this option only works when " +
+        "If partial aggregation aggregationPct greater than this value, "
+          + "partial aggregation may be early abandoned. Note: this option only works when " +
           "flushable partial aggregation is enabled. Ignored when " +
           "spark.gluten.sql.columnar.backend.velox.flushablePartialAggregation=false.")
       .intConf
@@ -1838,8 +1838,8 @@ object GlutenConfig {
     buildConf("spark.gluten.sql.columnar.backend.velox.abandonPartialAggregationMinRows")
       .internal()
       .doc(
-        "If partial aggregation aggregationPct greater than this value, "
-          + "partial aggregation may be early abandoned. Note: this option only works when " +
+        "If partial aggregation input rows number greater than this value, "
+          + " partial aggregation may be early abandoned. Note: this option only works when " +
           "flushable partial aggregation is enabled. Ignored when " +
           "spark.gluten.sql.columnar.backend.velox.flushablePartialAggregation=false.")
       .intConf
