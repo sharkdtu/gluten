3c440aaa041b2f2ceeeb304a6d83793d091d05dc
shamirchen <shamirchen@tencent.com>
2024-10-14 13:40:39 +0000

    Revert "[GLUTEN-6887][VL] Daily Update Velox Version (2024_10_08) (#7422)"
    This reverts commit e495aa4a7cbe52df845cbd5e01b9fa2e52660698.

--- cpp/velox/memory/VeloxMemoryManager.cc
+++ cpp/velox/memory/VeloxMemoryManager.cc
@@ -111,7 +111,7 @@ class ListenableArbitrator : public velox::memory::MemoryArbitrator {
   }
 
   uint64_t shrinkCapacity(uint64_t targetBytes, bool allowSpill, bool allowAbort) override {
-    velox::memory::ScopedMemoryArbitrationContext ctx();
+    velox::memory::ScopedMemoryArbitrationContext ctx((const velox::memory::MemoryPool*)nullptr);
     facebook::velox::exec::MemoryReclaimer::Stats status;
     velox::memory::MemoryPool* pool;
     {
